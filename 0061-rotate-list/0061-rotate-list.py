0667a258862032f0649a312918ad1438ab0dbbdc
libin2122 <pappypelushiya@gmail.com>
2026-07-23 09:01:27 +0530

    Time: 4 ms (5.78%), Space: 19.3 MB (75.85%) - LeetHub

---
+++ 0061-rotate-list/0061-rotate-list.py
@@ -0,0 +1,26 @@
+class Solution:
+    def rotateRight(self, head: ListNode, k: int) -> ListNode:
+        
+        if not head:
+            return None
+        
+        lastElement = head
+        length = 1
+        while ( lastElement.next ):
+            lastElement = lastElement.next
+            length += 1
+
+        k = k % length
+            
+     
+        lastElement.next = head
+        
+ 
+        tempNode = head
+        for _ in range( length - k - 1 ):
+            tempNode = tempNode.next
+        
+        answer = tempNode.next
+        tempNode.next = None
+        
+        return answer
